3e1cd29608ab07ebb351110e7ef9b2cd93a76307
john beltrez <beltrezjohn@gmail.com>
2020-07-19 05:38:34 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,7 +22,18 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-		
+		monopoly = {
+  :railroads=>
+  {
+    :pieces=>4,
+    :rent_in_dollars=>
+    {
+    },
+    :names=>
+    {
+    }
+  }
+}
 end
 
 def monopoly_with_fourth_tier
